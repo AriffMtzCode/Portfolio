29514b4549fa1e0c5c80fd884eea391c24f2ed22
Ariff Martinez <ariff.mtz.dev@gmail.com>
2023-05-02 10:46:18 -0600

    Gallery Component Progress

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,24 +1,134 @@
-import { Header } from "../ui/Header"
+import { Header } from "../ui/Header";
+import { Gallery } from "../ui/ResumePage/Gallery";
 
 export const ResumePage = () => {
   return (
     <div className="">
-      <Header text='Resumen'/>
-      <section>
-        <p className='text-white text-xl font-semibold ml-8 mt-8'>Educación</p>
-        <hr className='w-10 border border-primary-color ml-8'/>
-        <div>
+      <Header text="Resumen" />
+      <section className="h-auto bg-bg-page">
+        <p className="text-white text-xl font-semibold ml-8 mt-8">Educación</p>
+        <hr className="w-10 border border-primary-color ml-8" />
+        <div className="flex flex-col items-center mt-8">
           <div className="w-72 border-b border-l border-bg-box h-52">
-
+            <div className="flex items-center mt-2">
+              <p className="border-2 border-primary-color rounded-2xl w-12 p-1 text-xs text-center text-text-color mr-2 ml-[-0.5rem] bg-bg-footer">
+                2021
+              </p>
+              <p className="text-medium-text text-xs ">Udemy Courses</p>
+            </div>
+            <div className="ml-12">
+              <p className="text-sm text-white font-medium">
+                Master en CSS:Responsive, SASS, Flexbox, Grid y Bootstrap
+              </p>
+              <p className="text-xs text-text-color leading-6">
+                Aquí aprendí las bases de la maquetación web, así cómo
+                diferentes Frameworks de CSS para facilitar el trabajo de
+                diseñar y poder tener mejor calidad de código
+              </p>
+            </div>
           </div>
-          <div>
-
+          <div className="w-72 border-b border-l border-bg-box h-52">
+            <div className="flex items-center mt-2">
+              <p className="border-2 border-primary-color rounded-2xl w-12 p-1 text-xs text-center text-text-color mr-2 ml-[-0.5rem] bg-bg-footer">
+                2021
+              </p>
+              <p className="text-medium-text text-xs ">Udemy Courses</p>
+            </div>
+            <div className="ml-12">
+              <p className="text-sm text-white font-medium">
+                Git y Terminal de Comandos
+              </p>
+              <p className="text-xs text-text-color leading-6">
+                Aprendí el uso de la terminal de comandos, además de como
+                gestionar un proyecto usando Git para llevar un control de
+                versiones y una documentación.
+              </p>
+            </div>
           </div>
-          <div>
-
+          <div className="w-72 border-l border-bg-box h-52 mb-8">
+            <div className="flex items-center mt-2">
+              <p className="border-2 border-primary-color rounded-2xl w-12 p-1 text-xs text-center text-text-color mr-2 ml-[-0.5rem] bg-bg-footer">
+                2020
+              </p>
+              <p className="text-medium-text text-xs ">Udemy Courses</p>
+            </div>
+            <div className="ml-12">
+              <p className="text-sm text-white font-medium">
+                JavaScript Moderno
+              </p>
+              <p className="text-xs text-text-color leading-6">
+                Un curso donde aprendí la nueva sintaxis del ECMAScript. Además
+                de las mejores prácticas en el código, para que sea intuitivo y
+                escalable.
+              </p>
+            </div>
           </div>
         </div>
       </section>
+      <section className="h-auto bg-bg-page ">
+        <p className="text-white text-xl font-semibold ml-8">Experiencia</p>
+        <hr className="w-10 border border-primary-color ml-8" />
+        <div className="flex flex-col items-center mt-8">
+          <div className="w-72 border-b border-l border-bg-box h-52">
+            <div className="flex items-center mt-2">
+              <p className="border-2 border-primary-color rounded-2xl w-12 p-1 text-xs text-center text-text-color mr-2 ml-[-0.5rem] bg-bg-footer">
+                2021
+              </p>
+              <p className="text-medium-text text-xs ">Agencia Digital</p>
+            </div>
+            <div className="ml-12">
+              <p className="text-sm text-white font-medium">
+                Junior Web Development
+              </p>
+              <p className="text-xs text-text-color leading-6">
+                Estuve trabajando durante 1 año en una Agencia de Marketing
+                Digital como Desarrollador Web Junior, con tecnologías como:
+                Wordpress, Woocomerce, HTML, CSS y JavaScript
+              </p>
+            </div>
+          </div>
+          <div className="w-72 border-b border-l border-bg-box h-52">
+            <div className="flex items-center mt-2">
+              <p className="border-2 border-primary-color rounded-2xl w-12 p-1 text-xs text-center text-text-color mr-2 ml-[-0.5rem] bg-bg-footer">
+                2021
+              </p>
+              <p className="text-medium-text text-xs ">Freelancer</p>
+            </div>
+            <div className="ml-12">
+              <p className="text-sm text-white font-medium">UI Designer</p>
+              <p className="text-xs text-text-color leading-6">
+                Como FreeLancer estuve en proyectos donde requerían solo el
+                diseño de las interfaces además de diseños extras como logos,
+                banner etc. con tecnologías como: Photoshop, Ilustrador, Premier
+                Pro.
+              </p>
+            </div>
+          </div>
+          <div className="w-72 border-l border-bg-box h-52">
+            <div className="flex items-center mt-2">
+              <p className="border-2 border-primary-color rounded-2xl w-12 p-1 text-xs text-center text-text-color mr-2 ml-[-0.5rem] bg-bg-footer">
+                2021
+              </p>
+              <p className="text-medium-text text-xs ">Agencia Digital</p>
+            </div>
+            <div className="ml-12">
+              <p className="text-sm text-white font-medium">SEO</p>
+              <p className="text-xs text-text-color leading-6">
+                Un rol importante que también tuve fue optimizar todos los
+                sitios webs para el motor de búsqueda consiguiendo mayor
+                visualización.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="h-auto bg-bg-page ">
+      <p className="text-white text-xl font-semibold ml-8">Certificados</p>
+        <hr className="w-10 border border-primary-color ml-8" />
+        <div>
+          <Gallery />
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
---
+++ src/ui/ResumePage/Gallery.jsx
@@ -0,0 +1,127 @@
+import { HiOutlineCode } from "react-icons/hi";
+import { AiOutlineHtml5, AiFillGithub } from "react-icons/ai";
+import { DiCss3, DiJavascript } from "react-icons/di";
+import { BsTerminal } from "react-icons/bs";
+
+import Github from "../../assets/certifications/githubCertification.jpg";
+import HtmlP1 from "../../assets/certifications/htmlP1.jpg";
+import HtmlP2 from "../../assets/certifications/htmlP2.jpg";
+import HtmlP3 from "../../assets/certifications/htmlP3.jpg";
+import HtmlP4 from "../../assets/certifications/htmlP4.jpg";
+import LogicConcepts from "../../assets/certifications/logicaConceptosPrimordiales.jpg";
+import LogicPractice from "../../assets/certifications/logicaPracticandoConJuegosAnimaciones.jpg";
+import LogicSteps from "../../assets/certifications/logicaPrimerosPasos.jpg";
+import MasterCSS from "../../assets/certifications/masterCSS.jpg";
+import JSModerno from "../../assets/certifications/jsModerno.jpg";
+import TerminalComands from "../../assets/certifications/terminal.jpg";
+
+
+const certifications = [
+  {
+    nameCertification: "Lógica de programación - Primeros Pasos",
+    organization: "Alura",
+    finishDate: "05 de abril de 2023",
+    icon: <HiOutlineCode size={40} color={"#2196F3"} />,
+    image: Github,
+  },
+  {
+    nameCertification:
+      "Lógica de programación: Practicando con juegos y animaciones",
+    organization: "Alura",
+    finishDate: "06 de abril de 2023",
+    icon: <HiOutlineCode size={40} color={"#2196F3"} />,
+    image: HtmlP1,
+  },
+  {
+    nameCertification: "Lógica de programación: coceptos primordiales",
+    organization: "Alura",
+    finishDate: "05 de abril de 2023",
+    icon: <HiOutlineCode size={40} color={"#2196F3"} />,
+    image: HtmlP2,
+  },
+  {
+    nameCertification: "HTML5 y CSS3 parte 1: Mi primera página web",
+    organization: "Alura",
+    finishDate: "06 de abril de 2023",
+    icon: <AiOutlineHtml5 size={40} color={"#2196F3"} />,
+    image: HtmlP3,
+  },
+  {
+    nameCertification:
+      "HTML5 y CSS3 parte 2: Posicionamiento, listas y navegación",
+    organization: "Alura",
+    finishDate: "07 de abril de 2023",
+    icon: <AiOutlineHtml5 size={40} color={"#2196F3"} />,
+    image: HtmlP4,
+  },
+  {
+    nameCertification:
+      "HTML5 y CSS3 parte 3: Trabajando con formularios y tablas",
+    organization: "Alura",
+    finishDate: "07 de abril de 2023",
+    icon: <AiOutlineHtml5 size={40} color={"#2196F3"} />,
+    image: LogicConcepts,
+  },
+  {
+    nameCertification: "HTML5 y CSS3 parte 4: Avanzando en CSS",
+    organization: "Alura",
+    finishDate: "07 de abril de 2023",
+    icon: <AiOutlineHtml5 size={40} color={"#2196F3"} />,
+    image: LogicPractice,
+  },
+  {
+    nameCertification: "GIT y GITHUB: Controle y comparta su código",
+    organization: "Alura",
+    finishDate: "10 de abril de 2023",
+    icon: <AiFillGithub size={40} color={"#2196F3"} />,
+    image: LogicSteps,
+  },
+  {
+    nameCertification:
+      "Master en CSS: Responsive, SASS, Flexbox, Grid y Bootstrap",
+    organization: "Udemy Courses",
+    finishDate: "21 de Octubre de 2021",
+    icon: <DiCss3 size={40} color={"#2196F3"} />,
+    image: MasterCSS,
+  },
+  {
+    nameCertification:
+      "Curso de Git y la Terminal de comandos - Para programadores",
+    organization: "Udemy Courses",
+    finishDate: "10 de abril de 2023",
+    icon: <BsTerminal size={40} color={"#2196F3"} />,
+    image: TerminalComands,
+  },
+  {
+    nameCertification: "JavaScript Moderno: Guia para dominar el lenguaje   ",
+    organization: "Udemy Courses",
+    finishDate: "2 de Mayo de 2023",
+    icon: <DiJavascript size={40} color={"#2196F3"} />,
+    image: JSModerno,
+  },
+];
+
+export const Gallery = () => {
+  return (
+    <div className="flex flex-col justify-center items-center mt-8">
+      {certifications.map(
+        ({ nameCertification, finishDate, organization, icon, image }) => (
+          <div
+            key={nameCertification}
+            className="text-text-color h-full flex justify-center items-center border-2 border-box-border rounded-lg w-[80%] mb-4"
+          >
+            <div className="w-[120px] h-[120px] border-r-2 rounded-r-none rounded-lg border-box-border bg-bg-box flex items-center justify-center">
+              {icon}
+              <img src={image} />
+            </div>
+            <div className="w-full h-full flex flex-col justify-center ml-4">
+              <p className="text-sm text-white">{nameCertification}</p>
+              <p className="text-medium-text text-xs mt-2">{finishDate}</p>
+              <p className="text-medium-text text-xs">{organization}</p>
+            </div>
+          </div>
+        )
+      )}
+    </div>
+  );
+};
